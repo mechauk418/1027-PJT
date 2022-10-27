da76709c68085213c24ee5718b9e39b59f808855
mechauk418 <mechauk418@gmail.com>
2022-10-27 15:11:06 +0900

    accounts/signup

--- reviews/accounts/urls.py
+++ reviews/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path()
-    
+    path('',views.index,name = 'index'),
+    path('signup/',views.signup,name = 'signup')
 ]
