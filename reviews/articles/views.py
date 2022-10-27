9aeeb4292173b6f682b3c39a49e4f3da20fa66ad
jiyeong99 <1999wldud@gmail.com>
2022-10-27 15:10:27 +0900

    articles/detail

--- reviews/articles/views.py
+++ reviews/articles/views.py
@@ -1,6 +1,8 @@
 import imp
 from symbol import arith_expr
 from django.shortcuts import render, redirect
+
+from reviews import articles
 from .models import Article, Comment
 # Create your views here.
 def index(request):
@@ -9,3 +11,10 @@ def index(request):
         'articles' : articles,
     }
     return render(request, 'articles/index.html', context)
+
+def detail(request,review_pk):
+    article = Article.objects.get(pk=review_pk)
+    context = {
+        'article' : article,
+    }
+    return render(request, 'articles/detail.html')
