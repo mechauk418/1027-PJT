3bda447460136f49b05eb26363bbb0cf4f3e9fa7
jiyeong99 <1999wldud@gmail.com>
2022-10-27 15:32:00 +0900

    articles/create

--- reviews/articles/views.py
+++ reviews/articles/views.py
@@ -1,9 +1,7 @@
-import imp
-from symbol import arith_expr
 from django.shortcuts import render, redirect
-
-from reviews import articles
 from .models import Article, Comment
+from .forms import ArticleForm, CommentForm
+
 # Create your views here.
 def index(request):
     articles = Article.objects.all()
@@ -18,3 +16,17 @@ def detail(request,review_pk):
         'article' : article,
     }
     return render(request, 'articles/detail.html')
+
+def create(request):
+    if request.method=='POST':
+        form = ArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = ArticleForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'articles/forms.html', context)
+        
