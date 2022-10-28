115612ae0ecbba4a0fdd4d82134e6dab3c8a56d1
JC <chohy1106@gmail.com>
2022-10-28 11:23:56 +0900

    make comment, like

--- reviews/articles/models.py
+++ reviews/articles/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.conf import settings
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
+
 # Create your models here.
 
 class Article(models.Model):
@@ -11,20 +12,23 @@ class Article(models.Model):
     title = models.CharField(max_length=80)
     content = models.TextField()
     image = ProcessedImageField(
-        upload_to = 'images/',
-        blank = True, 
-        processors= [ResizeToFill(1200,960)],
-        format='JPEG',
-        options={'quality':90},
-        )
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+        upload_to="images/",
+        blank=True,
+        processors=[ResizeToFill(1200, 960)],
+        format="JPEG",
+        options={"quality": 90},
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     grade = models.FloatField()
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
-    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name = 'like_article')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="like_article"
+    )
+
 
 class Comment(models.Model):
 
     content = models.TextField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
-    article = models.ForeignKey(Article, on_delete = models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
--- reviews/articles/views.py
+++ reviews/articles/views.py
@@ -1,49 +1,92 @@
 from django.shortcuts import render, redirect
 from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 def index(request):
     articles = Article.objects.all()
     context = {
-        'articles' : articles,
+        "articles": articles,
     }
-    return render(request, 'articles/index.html', context)
+    return render(request, "articles/index.html", context)
 
-def detail(request,review_pk):
-    article = Article.objects.get(pk=review_pk)
+
+def detail(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
     context = {
-        'article' : article,
+        "article": article,
+        'comment_form': CommentForm(),
+        'comment': article.comment_set.all(),
     }
-    return render(request, 'articles/detail.html', context)
+    return render(request, "articles/detail.html", context)
+
 
 def create(request):
-    if request.method=='POST':
+    if request.method == "POST":
         form = ArticleForm(request.POST, request.FILES)
         if form.is_valid():
             forms = form.save(commit=False)
             forms.user = request.user
             forms.save()
-            return redirect('articles:index')
+            return redirect("articles:index")
     else:
         form = ArticleForm()
     context = {
-        'form' : form,
+        "form": form,
     }
-    return render(request, 'articles/forms.html', context)
-    
-def update(request,review_pk):
-    article = Article.objects.get(pk=review_pk)
-    if request.method=='POST':
+    return render(request, "articles/forms.html", context)
+
+
+def update(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.method == "POST":
         form = ArticleForm(request.POST, request.FILES, instance=article)
         if form.is_valid():
             forms = form.save(commit=False)
             forms.user = request.user
             forms.save()
-            return redirect('articles:detail', review_pk)
+            return redirect("articles:detail", article_pk)
     else:
         form = ArticleForm(instance=article)
     context = {
-        'form' : form,
+        "form": form,
     }
-    return render(request, 'articles/forms.html', context)
+    return render(request, "articles/forms.html", context)
+
+
+# @login_required
+def comment_create(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.article = article
+            comment.user = request.user
+            comment.save()
+            return redirect("articles:detail", article.pk)
+    return redirect("articles:detail", article.pk)
+
+
+# @login_required
+def comment_delete(request, article_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    if request.user == comment.user:
+        if request.method == "POST":
+            comment.delete()
+            return redirect("articles:detail", article_pk)
+    return redirect("articles:detail", article_pk)
+
+
+# @login_required
+def like(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if article.like_users.filter(id=request.user.id).exists():
+        article.like_users.remove(request.user)
+    else:
+        article.like_users.add(request.user)
+    return redirect("articles:detail", article_pk)
