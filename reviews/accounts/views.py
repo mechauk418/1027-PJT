203617aec98df8b4fcb79c7af01ca215761c40ce
mechauk418 <mechauk418@gmail.com>
2022-10-28 13:43:22 +0900

    accounts/follow

--- reviews/accounts/views.py
+++ reviews/accounts/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from .models import User
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 
@@ -63,3 +66,38 @@ def profile(request,pk):
         'user': user
     }
     return render(request, 'accounts/profile.html', context)
+
+
+def follow(request, pk):
+    user = get_object_or_404(get_user_model(), pk=pk)
+    if request.user == user:
+        return redirect('accounts:profile', pk)
+    if request.user in user.followers.all():
+        user.followers.remove(request.user)
+    else:
+        user.followers.add(request.user)
+    return redirect('accounts:profile', pk)
+
+# @require_POST
+# def follow(request,pk):
+#     if request.user.is_authenticated:
+#         User = get_user_model()
+#         me = request.user
+#         you = User.objects.get(pk=pk)
+
+#         if me != you:
+#             if you.followers.filter(pk=me.pk).exists():
+#                 you.followers.remove(me)
+#                 is_followed = False
+
+#             else:
+#                 you.followers.add(me)
+#                 is_followed = True
+
+#             context = {
+#                 'is_follow':is_followed,
+#             }
+#             return JsonResponse(context)
+
+#         return redirect('accounts:profile',you.username)
+#     return redirect('accounts:login')
